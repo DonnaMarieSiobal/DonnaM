627cb628314a871f1a39ce6c2721a90176fc4e1f
DonnaMarieSiobal <102038634+DonnaMarieSiobal@users.noreply.github.com>
2022-06-03 00:08:53 +0800

    Add files via upload

---
+++ favorite_city.py
@@ -0,0 +1,15 @@
+# A program that shows my favorite city
+def favorite_city(a="Quezon City", b="Ilolo City", c="Tagaytay"):
+    print(a)
+    print(b)
+    print(c)
+    name = str(c)
+    print("\nOne of my favorite cities is:", name)
+
+
+favorite_city()
+
+
+
+
+
---
+++ guessing game.py
@@ -0,0 +1,11 @@
+print("C’MON LET’S PLAY A GUESSING GAME! \n")
+
+favorite = "Bulalo"
+food = input("What's my favorite food?")
+
+if food == favorite:
+    print("Yep! So Amazing! \n")
+else:
+    print("Yuck! That's not it! \n")
+
+print("Thanks for playing!")
